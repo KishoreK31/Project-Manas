f4ba399400b5574755620861336c0cab3cb24ce3
KishoreK31 <43350913+KishoreK31@users.noreply.github.com>
2019-01-02 22:47:45 +0530

    Add files via upload

---
+++ Communication/I2C/slave1/slave1.ino
@@ -0,0 +1,19 @@
+#include "Wire.h"
+char x;
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(9);
+  Wire.onReceive(Event);
+}
+
+void Event(char bytes){
+  x = Wire.read();
+  Serial.print("The character that you entered in the other Arduino is ");
+  Serial.println(x);
+}
+
+void loop() {
+  delay(100);
+}
